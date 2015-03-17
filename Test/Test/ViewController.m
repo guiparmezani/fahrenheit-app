121ff963552db6da2deaae6d3f0fc6c38986da9a
Guilherme Parmezani <gparmezani@fahrenheitmarketing.com>
2015-03-17 10:43:28 -0500

    Renaming the App

--- Test/Test/ViewController.m
+++ Test/Test/ViewController.m
@@ -20,7 +20,7 @@
     [super viewDidLoad];
     //sample invoke code. 3 lines!
     PagedImageScrollView *pageScrollView = [[PagedImageScrollView alloc] initWithFrame:CGRectMake(0, 0, [[UIScreen mainScreen] applicationFrame].size.width, [[UIScreen mainScreen] applicationFrame].size.height + 40)];
-    [pageScrollView setScrollViewContents:@[[UIImage imageNamed:@"main.png"], [UIImage imageNamed:@"page-seo.png"], [UIImage imageNamed:@"page-responsiveness.png"], [UIImage imageNamed:@"page-online-marketing.png"]]];
+    [pageScrollView setScrollViewContents:@[[UIImage imageNamed:@"main.png"], [UIImage imageNamed:@"page-seo.png"], [UIImage imageNamed:@"page-responsiveness.png"], [UIImage imageNamed:@"page-online-marketing.png"], [UIImage imageNamed:@"page-end.png"]]];
     //easily setting pagecontrol pos, see PageControlPosition defination in PagedImageScrollView.h
     pageScrollView.pageControlPos = PageControlPositionCenterBottom;
     [self.view addSubview:pageScrollView];
